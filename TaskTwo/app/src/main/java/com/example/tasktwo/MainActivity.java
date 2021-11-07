f70c55ccd56b7e4eb5940352b6e28babeb6c838f
Muhammad Hassan Chattha <86921594+firehas123@users.noreply.github.com>
2021-11-07 16:18:10 +0500

    Task Two

--- TaskTwo/app/src/main/java/com/example/tasktwo/MainActivity.java
+++ TaskTwo/app/src/main/java/com/example/tasktwo/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.tasktwo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
+    TextView tx;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.Submit);
+        tx = findViewById(R.id.Number);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it =  new Intent(MainActivity.this,MainActivity2.class);
+                it.putExtra("Num", Integer.parseInt(tx.getText().toString()));
+                startActivity(it);
+
+            }
+        });
     }
+
+
+
 }
---
+++ TaskTwo/app/src/main/java/com/example/tasktwo/MainActivity2.java
@@ -0,0 +1,21 @@
+package com.example.tasktwo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+    TextView tx;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String value = extras.getString("Num");
+            //setting value
+        }
+    }
+}
