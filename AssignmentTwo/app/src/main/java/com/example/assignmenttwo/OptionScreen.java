861f293947a851bd1b3b129094bd0eb1a1d65f13
Muhammad Hassan Chattha <86921594+firehas123@users.noreply.github.com>
2021-11-08 02:38:02 +0500

    Second Page done

--- AssignmentTwo/app/src/main/java/com/example/assignmenttwo/OptionScreen.java
+++ AssignmentTwo/app/src/main/java/com/example/assignmenttwo/OptionScreen.java
@@ -3,12 +3,15 @@ package com.example.assignmenttwo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class OptionScreen extends AppCompatActivity {
-
+    Button exam;
+    Button practise;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_option_screen);
+        //setting on click event for both buttons
     }
 }
