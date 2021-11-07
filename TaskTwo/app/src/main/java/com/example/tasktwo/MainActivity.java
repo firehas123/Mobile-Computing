f03359e8369195629a50bb769dc61973933de048
Muhammad Hassan Chattha <86921594+firehas123@users.noreply.github.com>
2021-11-07 16:25:03 +0500

    Intent

--- TaskTwo/app/src/main/java/com/example/tasktwo/MainActivity.java
+++ TaskTwo/app/src/main/java/com/example/tasktwo/MainActivity.java
@@ -6,12 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn;
-    TextView tx;
+    EditText tx;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
