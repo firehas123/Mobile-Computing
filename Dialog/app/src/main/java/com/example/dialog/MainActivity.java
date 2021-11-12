55d0a98d43366bbe2b664f018744cf453c3d91a4
Muhammad Hassan Chattha <86921594+firehas123@users.noreply.github.com>
2021-11-12 20:33:34 +0500

    Commit 3

--- Dialog/app/src/main/java/com/example/dialog/MainActivity.java
+++ Dialog/app/src/main/java/com/example/dialog/MainActivity.java
@@ -5,37 +5,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //code for writing dialog box
-        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setMessage("It is message");
-        builder.setTitle("Title");
-        builder.setCancelable(false);
-        builder.setPositiveButton(
-                "Positive Button",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int asdf)
-                    {
-                        finish();
-                    }
-                });
-        builder.setNegativeButton(
-                "Negative Button",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which)
-                    {
-                        dialog.cancel();
-                    }
-                });
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //code for writing dialog box
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("It is message");
+                builder.setTitle("Title");
+                builder.setCancelable(false);
+                builder.setPositiveButton(
+                        "Positive Button",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int asdf)
+                            {
+                                finish();
+                            }
+                        });
+                builder.setNegativeButton(
+                        "Negative Button",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which)
+                            {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
+
     }
 }
