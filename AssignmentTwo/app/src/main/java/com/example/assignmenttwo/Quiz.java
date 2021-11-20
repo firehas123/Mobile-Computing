2a2d731377fd00d7e2c7b5238a94c866d17bb8f9
Muhammad Hassan Chattha <86921594+firehas123@users.noreply.github.com>
2021-11-20 22:13:13 +0500

    changes made in Quiz.java

--- AssignmentTwo/app/src/main/java/com/example/assignmenttwo/Quiz.java
+++ AssignmentTwo/app/src/main/java/com/example/assignmenttwo/Quiz.java
@@ -387,37 +387,3 @@ public class Quiz extends AppCompatActivity {
     }
 }
 
-class Question implements Parcelable {
-    MyData ob; //question
-    int index; //index of arrayList
-    public Question(MyData object,int i){
-        this.ob=object;
-        this.index=i;
-    }
-
-    protected Question(Parcel in) {
-        index = in.readInt();
-    }
-
-    public static final Creator<Question> CREATOR = new Creator<Question>() {
-        @Override
-        public Question createFromParcel(Parcel in) {
-            return new Question(in);
-        }
-
-        @Override
-        public Question[] newArray(int size) {
-            return new Question[size];
-        }
-    };
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeInt(index);
-    }
-}
