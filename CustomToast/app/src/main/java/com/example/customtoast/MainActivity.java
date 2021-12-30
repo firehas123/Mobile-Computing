f7c474e9bd6e30da7668be16e3e98e6c30a7f6f4
Muhammad Hassan Chattha <86921594+firehas123@users.noreply.github.com>
2021-12-30 15:46:48 +0500

    custom toast done

--- CustomToast/app/src/main/java/com/example/customtoast/MainActivity.java
+++ CustomToast/app/src/main/java/com/example/customtoast/MainActivity.java
@@ -21,7 +21,16 @@ public class MainActivity extends AppCompatActivity {
         customToast.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                //Creating the LayoutInflater instance
+                LayoutInflater li = getLayoutInflater();
+                //Getting the View object as defined in the customtoast.xml file
+                View layout = li.inflate(R.layout.custom_toast,(ViewGroup) findViewById(R.id.my_custom_toast));
+                //Creating the Toast object
+                Toast toast = new Toast(getApplicationContext());
+                toast.setDuration(Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                toast.setView(layout);//setting the view of custom toast layout
+                toast.show();
             }
         });
     }
