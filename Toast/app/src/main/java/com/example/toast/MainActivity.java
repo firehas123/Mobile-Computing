5c9c5b59956e5ba0a9f4850c716d124bdac1f3ce
Muhammad Hassan Chattha <86921594+firehas123@users.noreply.github.com>
2022-01-01 17:16:53 +0500

    done

--- Toast/app/src/main/java/com/example/toast/MainActivity.java
+++ Toast/app/src/main/java/com/example/toast/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast;
+                toast = Toast.makeText(MainActivity.this,"Hello my toast",Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER,100,200);
+                toast.show();
+            }
+        });
     }
 }
