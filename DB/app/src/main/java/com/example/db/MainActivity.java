06440cf353ab056a690795c7869d7fc5b8f11614
Muhammad Hassan Chattha <86921594+firehas123@users.noreply.github.com>
2022-01-07 03:40:54 +0500

    x

--- DB/app/src/main/java/com/example/db/MainActivity.java
+++ DB/app/src/main/java/com/example/db/MainActivity.java
@@ -2,14 +2,36 @@ package com.example.db;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Switch;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button butttonAdd, buttonView;
+    EditText editName, editAge;
+    Switch switchisActive;
+    ListView listViewDetail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        butttonAdd = findViewById(R.id.buttonAdd);
+        buttonView = findViewById(R.id.buttonView);
+        editName = findViewById(R.id.editTextName);
+        editAge = findViewById(R.id.editTextAge);
+        switchisActive = findViewById(R.id.switchisActive);
+        listViewDetail = findViewById(R.id.listViewDetails);
 
+        butttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
